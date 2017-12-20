34192dc8d66cc53fd36b829d19c5e7abb838f45e
Brandon Aguirre <brag4918@colorado.edu>
2017-12-20 15:50:18 -0700

    update config with 404 locations

--- Affogato/config.php
+++ Affogato/config.php
@@ -1,13 +1,14 @@
 <?php
 define('ROOT', preg_replace('/\/Affogato$/', '', __DIR__));
 define('APPS', ROOT . '/app');
-
+define('DEFAULT_NOT_FOUND', ROOT . '/Affogato/lib/404.php');
+// define('NOT_FOUND', APPS . '/404.php')
 
 define('CUSTOM_ROUTES', json_encode(array(
 	'index' => array(
 		'pattern' => '/^$/',
 		'path' => 'index.php'
-	), 
+	),
 	'Article' => array(
 		'pattern' => '/^article-([1-9][0-9]*)$/'
 	)
